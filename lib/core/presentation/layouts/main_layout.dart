8e6705dbd147595b9c636caa3bfa326b10cc043e
Raul antonio de la cruz hernandez <detretaty@gmail.com>
2026-08-12 14:07:46 -0600

    refactorización: reposicionar el botón de añadir en la barra de navegación del diseño principal

--- lib/core/presentation/layouts/main_layout.dart
+++ lib/core/presentation/layouts/main_layout.dart
@@ -54,9 +54,10 @@ class _MainLayoutState extends ConsumerState<MainLayout> {
                 _buildNavItem(Icons.home, Icons.home_outlined, 0),
                 _buildNavItem(Icons.autorenew, Icons.autorenew_outlined, 1),
                 _buildNavItem(Icons.handshake, Icons.handshake_outlined, 4),
-                _buildAddButton(context),
+
                 _buildNavItem(Icons.savings, Icons.savings_outlined, 2),
                 _buildNavItem(Icons.pie_chart, Icons.pie_chart_outline, 3),
+                                _buildAddButton(context),
               ],
             ),
           ),
